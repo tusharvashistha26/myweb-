05d50dd1c4a153e373ae5550ce187152da39b9d1
tusharvashistha26 <68603211+tusharvashistha26@users.noreply.github.com>
2022-10-02 15:22:38 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,8 @@
+arr1=[11,1,13,21,3,7]
+arr2=[11,3,7,1]
+ 
+if arr2 in arr1 :
+    print("arr1 is a subset of arr2")
+
+else :
+    print("arr1 is not a subset of arr2") 
---
+++ practice.py
@@ -0,0 +1,7 @@
+def per_1(name):
+    name=input("name of orgnization: ")
+    print(name+ " \n:thank")
+    return  
+    print("tu chutia") 
+per_1("ankit")    
+print(2+1)      
